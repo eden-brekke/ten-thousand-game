f217eed3eadbbbedd52cfc34ea07c05f1fd8107e
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-04-26 12:06:21 -0700

    added in class demo to pass quitter

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -1,2 +1,11 @@
+from ten_thousand.game_logic import GameLogic
+
+
 class Game:
-    pass
+    def play(self, roller=GameLogic.roll_dice):
+        print("Welcome to Ten Thousand")
+        print("(y)es to play or (n)o to decline")
+        response = input("> ")
+        if response == "n":
+            print("OK. Maybe another time")
+
